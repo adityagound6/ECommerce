3f06cc70e1e0102a9d374db7caa5c5b9221c931f
Aditya <adityagound6@gmail.com>
2022-08-24 13:07:40 +0530

    Admin project create

---
+++ ECommerce.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommerce.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
